898482c1b1a4ea022cf79ef79bd3582f2dd2583c
stockholm11 <a.can.bakir@gmail.com>
2025-05-06 18:49:15 +0300

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vip-ride-istanbul-airport/',
   build: {
     chunkSizeWarningLimit: 1000, // Increase the warning limit to 1000kb
     rollupOptions: {
