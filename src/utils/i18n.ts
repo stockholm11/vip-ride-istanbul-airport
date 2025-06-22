365fb285a0cebfded5c59d49d34404189c8fde11
stockholm11 <a.can.bakir@gmail.com>
2025-06-22 10:48:58 +0300

    feat: add i18n to the project

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 // Import translations
 import translationEN from '../locales/en/translation.json';
@@ -49,6 +50,7 @@ const resources = {
 };
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
